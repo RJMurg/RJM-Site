61e7141dabdc627d886c993c392906c787073d2d
Ruán <ruan.murgatroyd@sap.com>
2024-10-01 10:18:16 +0100

    [SK] Removed Lucia

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,27 +1,7 @@
-import { fail, redirect } from '@sveltejs/kit';
-import { lucia } from '$lib/server/auth';
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load = (async (event: Event) => {
-	if (!event.locals?.user) redirect(302, '/login');
-
+export const load = () => {
 	return {
-		hello: 'hello world!',
-		username: event.locals?.user?.username
+		hello: 'hello world!'
 	};
 }) satisfies PageServerLoad;
-
-export const actions: Actions = {
-	logout: async (event) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
-		await lucia.invalidateSession(event.locals.session.id);
-		const sessionCookie = lucia.createBlankSessionCookie();
-		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: '.',
-			...sessionCookie.attributes
-		});
-		redirect(302, '/login');
-	}
-};
