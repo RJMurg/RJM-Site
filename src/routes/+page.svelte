46eb8fa0cb84fa8f907bf3462cf497f9ca07435f
Ruán <ruan@murgatroyd.me>
2025-02-14 22:36:12 +0100

    [📈] Subtitle changes every 3 seconds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import Hero from '$lib/components/ui/hero/hero.svelte';
 
-	const subtitle = [
+	const subtitles = [
 		'Computer Science Student',
 		'Societies Forum President',
 		'WorldSkills Ireland Champion',
 		'VT/STAR Student @ SAP'
 	];
+
+	let tempSubtitles = subtitles;
+	let subtitle = updateSubtitle();
+
+	function updateSubtitle() {
+		const index = Math.floor(Math.random() * tempSubtitles.length);
+
+		const newSubtitle = tempSubtitles[index];
+		tempSubtitles = tempSubtitles.filter((_, i) => i !== index);
+
+		if (tempSubtitles.length === 0) {
+			tempSubtitles = subtitles;
+		}
+
+		return newSubtitle;
+	}
+
+	setInterval(() => {
+		subtitle = updateSubtitle();
+	}, 3000);
 </script>
 
-<Hero subtitle={subtitle[Math.floor(Math.random() * subtitle.length)]} />
+<Hero {subtitle} />
